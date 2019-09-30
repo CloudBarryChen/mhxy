8cd1c62a6013fa3881d069587f24a76fa7ef2751
CloudBarryChen <sadiluosi@163.com>
2019-09-30 17:41:47 +0800

    完善主页

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var dbHelper = require('../db/dbHelper.js');
 
 
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     // var filePath = path.resolve(__dirname,'../json','index.json');
     // fs.readFile(filePath,'UTF-8',function(err,data){
     //     if(err) throw err;
@@ -13,12 +13,25 @@ router.get('/',function(req,res){
     //     var newdata = JSON.parse(data);
     //     res.render('index.html',{'data':newdata.data});
     // })
-    dbHelper.AbilityModel.find(function(err,data){
+    /* dbHelper.AbilityModel.find(function(err,data){
         console.log(data);
         if(err) throw err;
         //var newdata = JSON.parse(data);
-        res.render('index.html',{'data':data});
+        res.render('index.html',{'abdata':data});
+    }) */
+    dbHelper.bbsModel.find(function (err, bbdata) {
+        // console.log(data);
+        if (err) throw err;
+        //var newdata = JSON.parse(data);
+        dbHelper.AbilityModel.find(function (err, abdata) {
+            // console.log(data);
+            if (err) throw err;
+            //var newdata = JSON.parse(data);
+            res.render('index.html', { 'bbdata': bbdata, 'abdata': abdata });
+        })
     })
+
+
 })
 
 module.exports = router;
